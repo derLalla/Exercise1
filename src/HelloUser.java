ac6dc222d10951598fd16313b6e10a4dc0ed0014
Rene <Rene@131.159.203.237>
2017-11-08 12:51:12 +0100

    HelloUser class was added
    hello_user branch was added

---
+++ src/HelloUser.java
@@ -0,0 +1,14 @@
+
+public class HelloUser {
+	private String userName;
+	
+	public HelloUser(String pUserName) {
+		userName = pUserName;
+	}
+	
+	public void greetUser() {
+		System.out.println("Hello " + userName +"!");
+	}
+	
+	
+}
